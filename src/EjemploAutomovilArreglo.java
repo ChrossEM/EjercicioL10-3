e465497b9f4cafb6295e2244cb4dc522bda94fa8
Chross_EM <cemc_cris@hotmail.com>
2023-10-04 11:58:08 -0600

    removed unused libraries

--- src/EjemploAutomovilArreglo.java
+++ src/EjemploAutomovilArreglo.java
@@ -1,8 +1,3 @@
-import javax.sound.sampled.AudioFileFormat;
-import java.sql.PseudoColumnUsage;
-import java.util.Arrays;
-import java.util.Objects;
-
 public class EjemploAutomovilArreglo {
     public static void main(String[] args) {
 
